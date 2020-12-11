7f77174319f28fbb8a23e2bf6fb18a29a0f3d39e
ckatr13 <53585660+ckatr13@users.noreply.github.com>
2020-12-11 16:42:44 -0500

    Updates

---
+++ precache-manifest.937f6d20c2c45e7a0af89685cc09209c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "941d0b82365650d00e164f9cef05bb2b",
+    "url": "/jammming/index.html"
+  },
+  {
+    "revision": "443e9dab5e7f0112bc37",
+    "url": "/jammming/static/css/main.2f5afcca.chunk.css"
+  },
+  {
+    "revision": "0d5f948a23485bb3eb79",
+    "url": "/jammming/static/js/2.155ba6da.chunk.js"
+  },
+  {
+    "revision": "443e9dab5e7f0112bc37",
+    "url": "/jammming/static/js/main.16008308.chunk.js"
+  },
+  {
+    "revision": "134fa62ff5d5bd01b72e",
+    "url": "/jammming/static/js/runtime~main.fce98cdf.js"
+  },
+  {
+    "revision": "37be7357a5f8583073df7e0aec787eaa",
+    "url": "/jammming/static/media/background_photo_desktop.37be7357.jpg"
+  }
+]);
